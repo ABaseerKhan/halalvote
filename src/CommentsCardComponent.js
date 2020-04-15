03c7695361938992b8e25d45592c8dfac0caf67c
Yahya Saad <yahya@ys-macbook-pro.fios-router.home>
2020-04-14 22:59:11 -0400

    Convert to functional components. Clean export usage

--- src/CommentsCardComponent.js
+++ src/CommentsCardComponent.js
@@ -1,91 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-class CommentComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: this.props.comment.username,
-      post: this.props.comment.post,
-      replies: this.props.comment.replies,
-      upVotes: this.props.comment.upVotes,
-      downVotes: this.props.comment.downVotes,
-      repliesHidden: true
-    }
-  }
+const CommentComponent = (props) => {
+  const [state, setState] = useState({
+    username: props.comment.username,
+    post: props.comment.post,
+    replies: props.comment.replies,
+    upVotes: props.comment.upVotes,
+    downVotes: props.comment.downVotes,
+    repliesHidden: true
+  });
 
-  flipRepliesHidden() {
-    this.setState((state, props) => {
-      return {
+  const flipRepliesHidden = () => {
+    setState({
+        ...state,
         repliesHidden: !state.repliesHidden
-      };
     });
   }
 
-  upVote() {
-    this.setState((state, props) => {
-      return {
+  const upVote = () => {
+    setState({
+        ...state,
         upVotes: state.upVotes + 1
-      }
     });
   }
 
-  downVote() {
-    this.setState((state, props) => {
-      return {
+  const downVote = () => {
+    setState({
+        ...state,
         downVotes: state.downVotes + 1
-      }
     });
   }
 
-  render() {
-    var replies = this.state.replies;
-    return(
-      <div>
-        <div className="username">{this.state.username}</div>
-        <div className="comment">{this.state.post}</div>
-        <div className="down-votes">{this.state.downVotes}</div>
-        <button className="down-vote-button" onClick={() => {this.downVote();}}>-</button>
-        <div className="up-votes">{this.state.upVotes}</div>
-        <button className="up-vote-button" onClick={() => {this.upVote();}}>+</button>
-        <br />
-        <div className="replies">
-          {
-            replies.map((reply, i) => {
-              if (!this.state.repliesHidden || i === 0) {
-                return <CommentComponent comment={reply} key={i}/>
-              } else {
-                return null
-              }
-            })
-          }
-        </div>
-        {
-          replies.length > 1 &&
-            <button className="show-more-button" onClick={() => {this.flipRepliesHidden();}}>
-              {
-                this.state.repliesHidden ? "Show More" : "Show Less"
-              }
-            </button>
-        }
-      </div>
-    )
-  }
-}
+  var replies = state.replies;
 
-class CommentsCardComponent extends React.Component {
-  render() {
-    var comments = this.props.comments;
-    return(
-      <div className={"card-" + this.props.direction}>
+  return (
+    <div>
+      <div className="username">{state.username}</div>
+      <div className="comment">{state.post}</div>
+      <div className="down-votes">{state.downVotes}</div>
+      <button className="down-vote-button" onClick={() => downVote()}>-</button>
+      <div className="up-votes">{state.upVotes}</div>
+      <button className="up-vote-button" onClick={() => upVote()}>+</button>
+      <br />
+      <div className="replies">
         {
-          comments.map((comment, i) => {
-            return <CommentComponent comment={comment} key={i}/>
+          replies.map((reply, i) => {
+            if (!state.repliesHidden || i === 0) {
+              return <CommentComponent comment={reply} key={i}/>
+            } else {
+              return null
+            }
           })
         }
       </div>
-    )
-  }
-}
+      {
+        replies.length > 1 &&
+          <button className="show-more-button" onClick={() => flipRepliesHidden()}>
+            {
+              state.repliesHidden ? "Show More" : "Show Less"
+            }
+          </button>
+      }
+    </div>
+  )
+};
 
-export default CommentsCardComponent
+export const CommentsCardComponent = (props) => {
+  var comments = props.comments;
+  return(
+    <div className={"card-" + props.direction}>
+      {
+        comments.map((comment, i) => {
+          return <CommentComponent comment={comment} key={i}/>
+        })
+      }
+    </div>
+  )
+}
