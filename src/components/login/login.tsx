00a15505ed7dae662cc88e6424cfe7c4d163b4f1
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-06-20 13:03:57 -0400

    Added register user

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -35,15 +35,52 @@ export const LoginComponent = (props: LoginComponentProps) => {
         fetchData();
     }
 
+    const registerUser = () => {
+        const fetchData = async () => {
+            const email = (document.getElementById("register-email-input") as HTMLInputElement).value
+            const username = (document.getElementById("register-username-input") as HTMLInputElement).value
+            const password = (document.getElementById("register-password-input") as HTMLInputElement).value
+
+            const fetchedUsername = await postData({
+                baseUrl: usersConfig.url,
+                path: 'register-user',
+                data: {
+                    "email": email,
+                    "username": username,
+                    "password": password,
+                },
+                additionalHeaders: { }
+            });
+
+            if (username == fetchedUsername) {
+                displayLogin(false);
+            }
+        }
+
+        fetchData();
+    }
+
     return (
         <div>
             <div className='login-cover' onClick={ () => { displayLogin(false) } }></div>
             <div className='login-modal'>
                 <div className='login-text'>Log In</div>
-                <div className="input-text-container">
+                <div>
+                    <label>Username</label>
                     <input id="username-input" type="text" placeholder="Username"/>
+                    <label>Password</label>
                     <input id="password-input" type="text" placeholder="Password"/>
-                    <button onClick={ () => { login() } }>Log In</button>
+                    <button className="login-submit-button" onClick={ () => { login() } }>Log In</button>
+                </div>
+                <div className="register-text">Register</div>
+                <div>
+                    <label>Email</label>
+                    <input id="register-email-input" type="text" placeholder="Email"/>
+                    <label>Username</label>
+                    <input id="register-username-input" type="text" placeholder="Username"/>
+                    <label>Password</label>
+                    <input id="register-password-input" type="text" placeholder="Password"/>
+                    <button className="login-submit-button" onClick={ () => { registerUser() } }>Register</button>
                 </div>
             </div>
         </div>
