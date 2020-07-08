b69c9ff6586fcf539472cf266e87a2040ae18d19
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-07-07 22:44:52 -0400

    Fixed

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -158,8 +158,8 @@ export const AppShellComponent = (props: any) => {
           analytics.style.paddingBottom = 120 - parseInt(analytics.style.paddingTop) + "px";
         }
 
-        commentsCardZero.style.height = `calc(80% - ${commentsTable.style.paddingTop} + 120px)`;
-        commentsCardOne.style.height = `calc(80% - ${commentsTable.style.paddingTop} + 120px)`;
+        commentsCardZero.style.height = `calc(60vh - ${commentsTable.style.paddingTop} + 120px)`;
+        commentsCardOne.style.height = `calc(60vh - ${commentsTable.style.paddingTop} + 120px)`;
 
       } else {
         menu.style.top = "0px";
@@ -167,8 +167,8 @@ export const AppShellComponent = (props: any) => {
         search.style.paddingTop = "120px";
         description.style.paddingTop = "120px";
         commentsTable.style.paddingTop = "120px";
-        commentsCardZero.style.height = "80%";
-        commentsCardOne.style.height = "80%";
+        commentsCardZero.style.height = "60vh";
+        commentsCardOne.style.height = "60vh";
         analytics.style.paddingTop = "120px";
         description.style.paddingBottom = "0px";
         commentsTable.style.paddingBottom = "0px";
