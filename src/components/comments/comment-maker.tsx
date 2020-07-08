bf294043deaa2468a52124236f5cc07ee87442ab
Yahya Saad <yahya@ys-macbook-pro.local>
2020-07-08 01:26:30 -0400

    submit comment on enter

--- src/components/comments/comment-maker.tsx
+++ src/components/comments/comment-maker.tsx
@@ -16,6 +16,9 @@ interface CommentMakerComponentProps {
 };
 
 export const CommentMakerComponent = (props: CommentMakerComponentProps) => {
+    const [state, setState] = useState({
+        holdingDownShift: false
+    });
     const [value, setValue] = useState('');
     const quillEditor = useRef<ReactQuill>(null);
 
@@ -28,6 +31,7 @@ export const CommentMakerComponent = (props: CommentMakerComponentProps) => {
             }
         }
     }, [props.replyToUsername])
+
     const submitComment = (event: any) => {
         if (value !== "") {
             event.preventDefault();
@@ -35,9 +39,47 @@ export const CommentMakerComponent = (props: CommentMakerComponentProps) => {
             setValue('');
         }
     };
+
+    const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        switch (event.keyCode) {	
+            case 13:
+                event.preventDefault();
+                if (!state.holdingDownShift) {
+                    submitComment(event as any);	
+                }	
+                break;	
+            case 16:	
+                setState({	
+                    ...state,	
+                    holdingDownShift: true	
+                });	
+            }	
+    }	
+
+
+    const onKeyUp = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.keyCode === 16) {	
+            setState({	
+                ...state,	
+                holdingDownShift: false	
+            });	
+        }	
+    };
+    
     return (
         <div className={"comment-maker-card-" + props.judgment} onClick={(e) => { e.stopPropagation()}}>
-            <ReactQuill ref={quillEditor} className={"comment-maker-input"} theme="snow" value={value} onChange={setValue} modules={modules} formats={formats} />
+            <ReactQuill 
+                ref={quillEditor} 
+                className={"comment-maker-input"} 
+                theme="snow" 
+                value={value} 
+                onChange={setValue} 
+                modules={modules} 
+                formats={formats} 
+                onKeyDown={onKeyDown}
+                onKeyUp={onKeyUp}
+                preserveWhitespace
+            />
             <SendButtonSVG className={"comment-maker-button-" + props.judgment} onClick={submitComment}/>
         </div>
     )
