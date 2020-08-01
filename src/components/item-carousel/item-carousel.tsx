a5aec090ff70d5726d5db6499dd3a8b46044a8ef
Yahya Saad <yahya@ys-macbook-pro.fios-router.home>
2020-08-01 17:34:30 -0400

    Style item-carousel chevrons

--- src/components/item-carousel/item-carousel.tsx
+++ src/components/item-carousel/item-carousel.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Linkify from 'react-linkify';
 import { ItemVotesComponent } from './item-votes';
+import { ReactComponent as ChevronLeftSVG } from '../../icons/chevron-left.svg';
+import { ReactComponent as ChevronRightSVG } from '../../icons/chevron-right.svg';
 
 // type imports
 
@@ -22,11 +24,15 @@ export const ItemCarouselComponent = (props: ItemCarouselComponentProps) => {
     return (
         <div id={id} style={props.style} className='item-carousel'>
             <div className="item-navigator">
-                <button onClick={iterateItem(-1)} className='carousel-button'>{"<"}</button>
+                <button onClick={iterateItem(-1)} className='carousel-button'>
+                    <ChevronLeftSVG className={"arrow-icon-left"}/>
+                </button>
                 <div className='item-text'>
                     <Linkify>{itemName}</Linkify>
                 </div>
-                <button onClick={iterateItem(1)} className='carousel-button'>{">"}</button>
+                <button onClick={iterateItem(1)} className='carousel-button'>
+                    <ChevronRightSVG className={"arrow-icon-right"}/>
+                </button>
             </div>
             <ItemVotesComponent itemName={itemName} userVote={userVote} halalVotes={halalVotes} haramVotes={haramVotes} addItemVoteLocally={addItemVoteLocally} />
         </div>
