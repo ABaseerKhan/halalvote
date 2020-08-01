b36f7443b8d5738c0cde38295ca8634261bc4721
Yahya Saad <yahya@ys-macbook-pro.fios-router.home>
2020-08-01 13:33:39 -0400

    Add keyboard up/down/enter events for search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -14,6 +14,7 @@ export const SearchComponent = (props: SearchComponentProps) => {
     const { id } = props;
     const { inputText, setInputText, searchResults } = useItemsSearch();
     const [autoCompleteOpen, setAutoCompleteOpen] = useState(false);
+    const [autoCompleteIndex, setAutoCompleteIndex] = useState<number>(-1);
 
     useEffect(() => {
         if (searchResults?.result?.data && searchResults?.result?.data.length) {
@@ -28,17 +29,43 @@ export const SearchComponent = (props: SearchComponentProps) => {
         document.getElementById('app-shell')?.scrollTo(0, window.innerHeight);
     };
 
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (!(searchResults?.result?.data || searchResults?.result?.data?.length)) {
+            return;
+        }
+        // arrow up/down button should select next/previous list element
+        if (e.keyCode === 38) {
+            e.preventDefault();
+            if (autoCompleteIndex > 0) setAutoCompleteIndex(prevIndex => (prevIndex - 1));
+        } else if (e.keyCode === 40) {
+            e.preventDefault();
+            if (autoCompleteIndex === -1) {
+                setAutoCompleteIndex(0);
+            }
+            else if (autoCompleteIndex < searchResults?.result?.data?.length - 1) {
+                setAutoCompleteIndex( prevIndex => (prevIndex + 1));
+            }
+        } else if (e.keyCode === 13) {
+            onClickSuggestion(searchResults.result.data[autoCompleteIndex][0])();
+        }
+    }
+
     return (
         <div id={id} className='search-page'>
             <div className={"search-bar"}>
                 <span className="search-header">--Logo--</span>
-                <input className={autoCompleteOpen ? "search-bar-input-autocomplete-open" : "search-bar-input"} type="text" value={inputText} onChange={e => setInputText(e.target.value)} />
+                <input 
+                    className={autoCompleteOpen ? "search-bar-input-autocomplete-open" : "search-bar-input"} 
+                    type="text" value={inputText} 
+                    onChange={e => setInputText(e.target.value)} 
+                    onKeyDown={onKeyDown}
+                />
                 <div className={"autocomplete"}>
                     {searchResults.result && searchResults.result.data && !!searchResults.result.data.length && (
                         <ul className={"autocomplete-list"}>
                             {
-                                searchResults.result.data.map((itemName: [string]) => (
-                                    <li className={"autocomplete-list-item"} key={itemName[0]}>
+                                searchResults.result.data.map((itemName: [string], index: number) => (
+                                    <li className={index===autoCompleteIndex ? "autocomplete-list-item-highlighted" : "autocomplete-list-item"} key={itemName[0]}>
                                         <div onClick={onClickSuggestion(itemName[0])} className={"suggestions-inner-container"}>
                                             <div className={"option"}>{itemName[0]}</div>
                                         </div>
