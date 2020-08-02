74a8f89ca1eaf8b59e328664b2ace4233869c19b
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-08-02 18:17:34 -0400

    Added ability to move add item button

--- src/components/add-item/add-item-button.tsx
+++ src/components/add-item/add-item-button.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ReactComponent as AddButtonSVG } from '../../icons/add-button.svg';
 import { ModalComponent } from '../modal/modal';
+import { useMedia } from '../../hooks/useMedia';
 
 // type imports
 import { ModalType } from '../../types';
@@ -13,12 +14,64 @@ interface AddItemButtonComponentProps {
 };
 export const AddItemButtonComponent = (props: AddItemButtonComponentProps) => {
     const { fetchItems } = props;
-    const [state, setState] = useState<{addItemDisplayed: Boolean  }>({
-        addItemDisplayed: false
+
+    const [state, setState] = useState<{addItemDisplayed: Boolean, isDragging: {value: boolean}  }>({
+        addItemDisplayed: false,
+        isDragging: {value: false}
     });
 
+    const isMobile = useMedia(
+        // Media queries
+        ['(max-width: 600px)'],
+        [true],
+        // default value
+        false
+    );
+
     const setAddItemDisplayed = (addItemDisplayed: Boolean) => {
-        setState({addItemDisplayed: addItemDisplayed});
+        setState({...state, addItemDisplayed: addItemDisplayed});
+    }
+
+    const addButtonId = "add-item-button";
+    const addButton = document.getElementById(addButtonId);
+
+    if (addButton) {
+        if (isMobile) {
+            addButton.ontouchmove = (event: any) => {
+                // grab the location of touch
+                var touchLocation = event.targetTouches[0];
+                
+                // assign box new coordinates based on the touch.
+                addButton.style.left = touchLocation.pageX - 30 + 'px';
+                addButton.style.top = touchLocation.pageY - 30 + 'px';
+            }
+            addButton.onclick = () => {
+                setAddItemDisplayed(true);
+            }
+        } else {
+            const onMouseMove = (event: any) => {
+                state.isDragging.value = true;
+                addButton.style.bottom = "0";
+                addButton.style.right = "0";
+                addButton.style.top = (parseInt(event.pageY) - 30).toString();
+                addButton.style.left = (parseInt(event.pageX) - 30).toString();
+            }
+            addButton.onmousedown = () => {
+                window.onmousemove = onMouseMove
+            };
+            addButton.onmouseup = () => {
+                window.onmousemove = null;
+                if (!state.isDragging.value) {
+                    setAddItemDisplayed(true);
+                } else {
+                    state.isDragging.value = false;
+                }
+            }
+            window.onmouseup = () => {
+                state.isDragging.value = false;
+                addButton.onmousemove = null;
+            }
+        }
     }
 
     return (
@@ -26,7 +79,7 @@ export const AddItemButtonComponent = (props: AddItemButtonComponentProps) => {
             { state.addItemDisplayed &&
                 <ModalComponent removeModal={() => setAddItemDisplayed(false)} modalType={ModalType.ADD_ITEM} fetchItems={fetchItems}/>
             }
-            <AddButtonSVG className="add-item-button" onClick={ () => setAddItemDisplayed(true) }></AddButtonSVG>
+            <AddButtonSVG id={addButtonId} className="add-item-button"></AddButtonSVG>
         </div>
     );
 }
