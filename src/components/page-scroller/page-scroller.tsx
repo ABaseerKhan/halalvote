70fa9a8842064408c2054639670dc23f56864b37
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-08-08 15:37:21 -0400

    Removed description from scrollable window

--- src/components/page-scroller/page-scroller.tsx
+++ src/components/page-scroller/page-scroller.tsx
@@ -8,28 +8,23 @@ interface PageScrollerComponentProps {
     pageZeroId: string,
     pageOneId: string,
     pageTwoId: string,
-    pageThreeId: string,
     scrollToPage: any;
 };
 export const PageScrollerComponent = (props: PageScrollerComponentProps) => {
-    const { pageZeroId, pageOneId, pageTwoId, pageThreeId, scrollToPage } = props;
+    const { pageZeroId, pageOneId, pageTwoId, scrollToPage } = props;
 
     return (
         <div className='page-scroller'>
             <div className='page-scroller-button-container'>
-                <div id={pageZeroId} className='page-scroller-button' onClick={() => {scrollToPage(0)}} data-tip={"Search"}/>
+                <div id={pageZeroId} className='page-scroller-button' onClick={() => {scrollToPage(0)}} data-tip={pageZeroId}/>
                 <ReactTooltip effect={"solid"} />
             </div>
             <div className='page-scroller-button-container'>
-                <div id={pageOneId} className='page-scroller-button-selected' onClick={() => {scrollToPage(1)}} data-tip={"Comments"}/>
+                <div id={pageOneId} className='page-scroller-button-selected' onClick={() => {scrollToPage(1)}} data-tip={pageOneId}/>
                 <ReactTooltip effect={"solid"} />
             </div>
             <div className='page-scroller-button-container'>
-                <div id={pageTwoId} className='page-scroller-button' onClick={() => {scrollToPage(2)}} data-tip={"Description"}/>
-                <ReactTooltip effect={"solid"} />
-            </div>
-            <div className='page-scroller-button-container'>
-                <div id={pageThreeId} className='page-scroller-button' onClick={() => {scrollToPage(3)}} data-tip={"Analytics"}/>
+                <div id={pageTwoId} className='page-scroller-button' onClick={() => {scrollToPage(2)}} data-tip={pageTwoId}/>
                 <ReactTooltip effect={"solid"} />
             </div>
         </div>
