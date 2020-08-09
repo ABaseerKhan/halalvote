770b78fef5319e9b6b0d932bfd1b9c3118ebb9c7
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-08-09 01:28:10 -0400

    Removed hover for item carousel buttons when on mobile

--- src/components/item-carousel/item-carousel.tsx
+++ src/components/item-carousel/item-carousel.tsx
@@ -3,6 +3,7 @@ import Linkify from 'react-linkify';
 import { ItemVotesComponent } from './item-votes';
 import { ReactComponent as ChevronLeftSVG } from '../../icons/chevron-left.svg';
 import { ReactComponent as ChevronRightSVG } from '../../icons/chevron-right.svg';
+import { useMedia } from '../../hooks/useMedia';
 
 // type imports
 
@@ -21,16 +22,36 @@ interface ItemCarouselComponentProps {
 };
 export const ItemCarouselComponent = (props: ItemCarouselComponentProps) => {
     const { id, iterateItem, itemName, userVote, halalPoints, haramPoints, numVotes } = props;
+    const isMobile = useMedia(
+        // Media queries
+        ['(max-width: 600px)'],
+        [true],
+        // default value
+        false
+    );
+
+    const leftCarouselButtonId = "left-carousel-button";
+    const rightCarouselButtonId = "right-carousel-button";
+
+    if (!isMobile) {
+        const leftCarouselButton = document.getElementById(leftCarouselButtonId);
+        const rightCarouselButton = document.getElementById(rightCarouselButtonId);
+        if (leftCarouselButton && rightCarouselButton) {
+            leftCarouselButton.classList.add("carousel-button-computer");
+            rightCarouselButton.classList.add("carousel-button-computer");
+        }
+    }
+
     return (
         <div id={id} style={props.style} className='item-carousel'>
             <div className="item-navigator">
-                <button onClick={iterateItem(-1)} className='carousel-button'>
+                <button id={leftCarouselButtonId} onClick={iterateItem(-1)} className='carousel-button'>
                     <ChevronLeftSVG className={"arrow-icon-left"}/>
                 </button>
                 <div className='item-text'>
                     <Linkify>{itemName}</Linkify>
                 </div>
-                <button onClick={iterateItem(1)} className='carousel-button'>
+                <button id={rightCarouselButtonId} onClick={iterateItem(1)} className='carousel-button'>
                     <ChevronRightSVG className={"arrow-icon-right"}/>
                 </button>
             </div>
