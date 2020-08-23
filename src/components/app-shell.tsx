70605ec9566c76a1ebbb66056db8a23da311e6f6
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-08-23 00:13:21 -0400

    Fixed warning

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -34,7 +34,7 @@ export const AppShellComponent = (props: any) => {
   }, []);
 
   useEffect(() => {
-    fetchItems(cookies.itemName || undefined);
+    fetchItems(cookies.itemName || undefined);// eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sessiontoken]);
 
   const fetchItems = async (itemTofetch?: string) => {
