47a0caaf37805800160db6ee28f6eddaccb4a0ba
Yahya Saad <yahya@ys-macbook-pro.local>
2020-11-04 17:07:31 -0500

    adjust comment skeleton styles for mobile

--- src/components/comments/comments-skeleton.tsx
+++ src/components/comments/comments-skeleton.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useMedia } from '../../hooks/useMedia';
 
-let SkeletonSections: JSX.Element[] = [];
-for (let i = 0; i < 10; i++) {
-    SkeletonSections.push(
-        <section style={{ margin: '20px' }}>
-            <Skeleton width={'30%'} style={{ margin: '5px', maxWidth: '200px' }} duration={.5} />
-            <Skeleton count={3} width={'90%'} style={{ margin: '5px' }} duration={.5} />
-        </section>
+export const SkeletonComponent = () => {
+    const isMobile = useMedia(
+        // Media queries
+        ['(max-width: 600px)'],
+        [true],
+        // default value
+        false
     );
-}
 
-export const SkeletonComponent = () => (
-    <SkeletonTheme color="#cccccc" highlightColor="#d9d9d9">
-        {SkeletonSections}
-    </SkeletonTheme>
-);
+    let SkeletonSections: JSX.Element[] = [];
+    for (let i = 0; i < 10; i++) {
+        SkeletonSections.push(
+            <section style={{ margin: '20px' }}>
+                <Skeleton height={isMobile ? '10px' : 'unset'} width={'30%'} style={{ margin: '5px', maxWidth: '200px' }} duration={.5} />
+                <Skeleton height={isMobile ? '10px' : 'unset'} count={3} width={'90%'} style={{ margin: '5px' }} duration={.5} />
+            </section>
+        );
+    }
+
+    return (
+        <SkeletonTheme color="#cccccc" highlightColor="#d9d9d9">
+            {SkeletonSections}
+        </SkeletonTheme>
+    )
+};
