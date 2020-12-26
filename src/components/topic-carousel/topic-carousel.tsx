85850f1f819c4b7bce70877bf5b9a721da0d1b11
Yahya Saad <yahya@ys-macbook-pro.local>
2020-12-26 18:55:37 -0500

    fix menu button position calc

--- src/components/topic-carousel/topic-carousel.tsx
+++ src/components/topic-carousel/topic-carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 import { TopicVotesComponent } from './topic-votes';
 import { topicsContext } from '../app-shell';
 
