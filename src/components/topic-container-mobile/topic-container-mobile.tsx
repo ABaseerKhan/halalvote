50b60022512b96023c1bdbde9d5eb907b7a5e8f9
AbdulBaseer Khan <baseer@AbdulBaseers-MacBook-Pro-2.local>
2020-12-27 00:02:49 -0500

    Sped up topic switching on mobile

--- src/components/topic-container-mobile/topic-container-mobile.tsx
+++ src/components/topic-container-mobile/topic-container-mobile.tsx
@@ -5,7 +5,7 @@ import { TopicExposeComponent } from './topic-expose';
 // styles
 import './topic-container-mobile.css';
 
-const TOPIC_SWITCHING_DURATION = 300;
+const TOPIC_SWITCHING_DURATION = 150;
 
 interface TopicContainerMobileComponentProps {
     fetchTopics: (topicTofetch?: string | undefined, newIndex?: number | undefined) => Promise<void>;
