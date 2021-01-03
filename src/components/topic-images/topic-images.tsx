40818d7689b57fe38473449748f567b3dbd6c23d
Yahya Saad <yahya@ys-macbook-pro.local>
2021-01-02 20:32:04 -0500

    Prevent multi-file upload

--- src/components/topic-images/topic-images.tsx
+++ src/components/topic-images/topic-images.tsx
@@ -337,6 +337,7 @@ export const MyUploader = (props: UploaderProps) => {
 
     return (
         <Dropzone
+            multiple={false}
             getUploadParams={getUploadParams}
             onChangeStatus={handleChangeStatus}
             onSubmit={handleSubmit}
