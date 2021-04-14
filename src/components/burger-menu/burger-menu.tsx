d265679b21b15261fe4caf45017ce78569df8c9f
charp2 <yahyabinsaad@gmail.com>
2021-04-14 17:46:20 -0400

    data policy

--- src/components/burger-menu/burger-menu.tsx
+++ src/components/burger-menu/burger-menu.tsx
@@ -1,6 +1,6 @@
 
 import { slide as Menu } from 'react-burger-menu'
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ModalComponent } from '../modal/modal';
 import { useCookies } from 'react-cookie';
 import { Portal } from '../../index';
@@ -15,6 +15,7 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import InfoIcon from '@material-ui/icons/Info';
 import InputIcon from '@material-ui/icons/Input';
 import PersonIcon from '@material-ui/icons/Person';
+import LockIcon from '@material-ui/icons/Lock';
 
 // type imports
 import { ModalType } from '../../types';
@@ -43,6 +44,14 @@ export const BurgerMenuComponent = (props: BurgerMenuComponentProps) => {
         modalItemSelected: undefined,
     });
 
+    const menuBottomRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (menuBottomRef?.current) {
+            menuBottomRef.current.classList.remove("bm-item");
+        }
+    }, [])
+
     useEffect(() => {
         if (query.has('loginScreen')) {
             state.modalItemSelected !== ModalType.LOGIN && setState({...state, modalItemSelected: ModalType.LOGIN});
@@ -52,7 +61,9 @@ export const BurgerMenuComponent = (props: BurgerMenuComponentProps) => {
             state.modalItemSelected !== ModalType.CONTACT && setState({...state, modalItemSelected: ModalType.CONTACT});
         } else if (query.has('about')) {
             state.modalItemSelected !== ModalType.ABOUT && setState({...state, modalItemSelected: ModalType.ABOUT});
-        } else if (state.modalItemSelected === ModalType.LOGIN || state.modalItemSelected === ModalType.PROFILE || state.modalItemSelected === ModalType.CONTACT || state.modalItemSelected === ModalType.ABOUT) {
+        } else if (query.has('dataPolicy')) {
+            state.modalItemSelected !== ModalType.DATA_POLICY && setState({...state, modalItemSelected: ModalType.DATA_POLICY});
+        } else if (state.modalItemSelected === ModalType.LOGIN || state.modalItemSelected === ModalType.PROFILE || state.modalItemSelected === ModalType.CONTACT || state.modalItemSelected === ModalType.DATA_POLICY || state.modalItemSelected === ModalType.ABOUT) {
             setState({...state, modalItemSelected: undefined});
         } // eslint-disable-next-line
     }, [history, query]);
@@ -117,6 +128,19 @@ export const BurgerMenuComponent = (props: BurgerMenuComponentProps) => {
                     }
                 }
                 break;
+            case ModalType.DATA_POLICY:
+                if (additionalQuery) {
+                    if (query.has('dataPolicy')) {
+                        query.set('dataPolicy', additionalQuery);
+                    } else {
+                        query.append('dataPolicy', additionalQuery);
+                    };
+                } else {
+                    if (query.has('dataPolicy')) {
+                        query.delete('dataPolicy');
+                    }
+                }
+                break;
             default:
                 return;
         }
@@ -169,12 +193,15 @@ export const BurgerMenuComponent = (props: BurgerMenuComponentProps) => {
                 onStateChange={(newState) => setMenuOpenState(newState.isOpen)}
             >
                 <li className="menu-item" onClick={goHome}><HomeIcon style={{marginRight: "10px"}}/> Home</li>
-                <li className="menu-item" onClick={() => { setMenuOpenState(false); setAddTopicDisplayed(true); }}><AddIcon style={{marginRight: "10px"}}/> Add topic</li>
-                {usernameExists() && <li className="menu-item" onClick={() => { setMenuOpenState(false); setAccountDisplayed(true); }}><AccountBoxIcon style={{marginRight: "10px"}}/> Account</li>}
+                <li className="menu-item" style={{ borderBottom: '1px solid gray' }} onClick={() => { setMenuOpenState(false); setAddTopicDisplayed(true); }}><AddIcon style={{marginRight: "10px"}}/> Add topic</li>
                 <li className="menu-item" onClick={() => { setMenuOpenState(false); updateUrl(ModalType.CONTACT, "shown"); }}><ContactMailIcon style={{marginRight: "10px"}}/> Contact us</li>
                 <li className="menu-item" onClick={() => { setMenuOpenState(false); updateUrl(ModalType.ABOUT, "shown"); }}><InfoIcon style={{marginRight: "10px"}} /> About</li>
-                <li className="menu-item" onClick={login}>{ usernameExists() ? <ExitToAppIcon style={{marginRight: "10px"}}/> : <InputIcon style={{marginRight: "10px"}}/> }  { usernameExists() ? "Logout" : "Login" }</li>
-                {usernameExists() && <div className={"username-tile"} onClick={() => { setMenuOpenState(false); updateUrl(ModalType.PROFILE, username); }}><PersonIcon style={{marginRight: "10px"}}/> {username}</div>}
+                <li className="menu-item" style={{ borderBottom: '1px solid gray' }} onClick={() => { setMenuOpenState(false); updateUrl(ModalType.DATA_POLICY, "shown"); }}><LockIcon style={{marginRight: "10px"}}/>Data policy</li>
+                <div ref={menuBottomRef} className="menu-item-bottom-section">
+                    {usernameExists() && <div className={"bm-item menu-item"} onClick={() => { setMenuOpenState(false); updateUrl(ModalType.PROFILE, username); }}><PersonIcon style={{marginRight: "10px"}}/> {username}</div>}
+                    {usernameExists() && <li className="bm-item menu-item" onClick={() => { setMenuOpenState(false); setAccountDisplayed(true); }}><AccountBoxIcon style={{marginRight: "10px"}}/>Account</li>}
+                    <li className="bm-item menu-item" onClick={login}>{ usernameExists() ? <ExitToAppIcon style={{marginRight: "10px"}}/> : <InputIcon style={{marginRight: "10px"}}/> }  { usernameExists() ? "Logout" : "Login" }</li>
+                </div>
             </Menu>
             {
                 state.modalItemSelected ===  ModalType.LOGIN ?
@@ -187,8 +214,10 @@ export const BurgerMenuComponent = (props: BurgerMenuComponentProps) => {
                     <Portal><ModalComponent removeModal={() => setAccountDisplayed(false)} modalType={ModalType.ACCOUNT}/></Portal> :
                 state.modalItemSelected === ModalType.CONTACT ?
                     <Portal><ModalComponent removeModal={() => updateUrl(ModalType.CONTACT, undefined)} modalType={ModalType.CONTACT}/></Portal> :
-                state.modalItemSelected === ModalType.ABOUT &&
-                    <Portal><ModalComponent removeModal={() => updateUrl(ModalType.ABOUT, undefined)} modalType={ModalType.ABOUT}/></Portal>
+                state.modalItemSelected === ModalType.ABOUT ?
+                    <Portal><ModalComponent removeModal={() => updateUrl(ModalType.ABOUT, undefined)} modalType={ModalType.ABOUT}/></Portal> :
+                state.modalItemSelected === ModalType.DATA_POLICY &&
+                    <Portal><ModalComponent removeModal={() => updateUrl(ModalType.DATA_POLICY, undefined)} modalType={ModalType.DATA_POLICY}/></Portal>
             }
         </>
     );
---
+++ src/components/data-policy/data-policy.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./data-policy.css";
+
+export const DataPolicyComponent = () => {
+  return (
+    <div className="data-policy-container">
+      <div>
+      Privacy Notice
+The Privacy Notice was last updated on 11 Apr, 2021.<br/><br/>
+<h3>Questions or concerns</h3>
+If you have any questions or concerns regarding this Privacy Notice, please send us a detailed message to mail@halalvote.com, and we will try to resolve your concerns.
+<br/><br/>
+<h3>1. Who is the Personal Information collector</h3>
+halalvote.com is the data controller (Controller) and responsible for your Personal Information. 
+<br/><br/>
+<h3>2. What Personal Information we collect about you and when we collect it</h3>
+<br/>
+<table>
+    <th>
+        WHEN INFORMATION IS COLLECTED
+    </th>
+    <th>
+        TYPE OF INFORMATION
+    </th>
+    <tr>
+        <td>When you register</td>
+        <td>Email, Username, Password</td>
+    </tr>
+</table>
+
+<br/><br/>
+
+<h3>3. How and with whom we share the Personal Information we collect</h3><br/>
+We neither rent nor sell your Personal Information in personally identifiable form to anyone.
+
+<br/><br/>
+<h3>4. How we store and secure the Personal Information we collect</h3>
+<br/>
+We endeavor to protect the privacy of your account and other Personal Information we hold in our records.
+<br/><br/>
+Sensitive information such as passwords are stored in encrypted form. SSL is applied when transferring information and while you access the Services from a web browser.
+<br/><br/>
+
+<h3>5. What rights do you, as the data subject, have</h3>
+<ul style={{ textAlign: 'left' }}>
+    <li>The right of access</li>
+    <li>The right to correction</li>
+    <li>The right to erasure</li>
+</ul>
+      </div>
+    </div>
+  )
+}
