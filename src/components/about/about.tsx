834085bb21e41d55166e733e69ab3bf835def8ca
charp2 <yahyabinsaad@gmail.com>
2021-04-26 19:54:33 -0400

    remove founders

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -6,9 +6,6 @@ export const AboutComponent = () => {
     <div className="about-container">
       <h4>Your source for everything halal and haram! A platform for muslims to get a community sentiment on whether various topics are viewed as halal or haram.</h4>
       <br/>
-      <h4>Founders:</h4>
-      <a href="https://www.linkedin.com/in/abdulbaseer-khan-928997124/">Abdulbaseer Khan</a>
-      <a href="https://www.linkedin.com/in/yahya-saad-3674829a/" style={{marginLeft: "10px"}}>Yahya Saad</a>
     </div>
   )
 }
